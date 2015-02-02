9eaaa8e73374e5d036bfd0a5b23201189e1ef1e6
robz <rjnevels@utexas.edu>
2015-02-02 07:03:01 -0800

    added more es6 tests, replaced es6 flag with harmony flag

--- src/main.js
+++ src/main.js
@@ -1,19 +1,42 @@
 /* @flow */
 
-// CommonJS test
-var React = require('react');
+// ES6 tests (commented out the ones that don't work yet)
+// arrow functions
+var f = (a, b) => a + b;
 
-// ES6 test
-var f = (y) => y+3;
+// array spread operator
+//var arr = [1, 2];
+//var arr2 = [0, ...arr];
+
+// argument spread operator
+//var arr = [1, 2];
+//var res = f(...arr);
+
+// rest parameter
+(function (a, ...args) { 
+  return a + args[0] + args[1]; 
+}(1, 2, 3));
+
+// destructuring
+(function ({a, b}) { return a + b; })({a:1, b:2});
+var [x, y] = [1, 2];
+
+// dynamic object keys
+//var a = {b:1};
+//var c = {a.b: 0};
+
 
 // flow test
 function sum(a: number, b: number): number {
   return a + b;
 }
 
+// CommonJS test
+var React = require('react');
+module.exports = sum;
+
+
 // JSX test
 var x = <div>hi</div>;
 
 console.log('yay!');
-
-module.exports = sum;
