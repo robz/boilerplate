c07dcb267ef7105d7853d271aaba2fbf1346face
robz <rjnevels@utexas.edu>
2015-02-07 14:16:03 -0800

    reorganized main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 /* @flow */
 
+// flow test
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+// CommonJS test
+var React = require('react');
+module.exports = sum;
+
+// JSX test
+var x = <div>hi</div>;
+
 // ES6 tests (commented out the ones that don't work yet)
+// classes
+class A { f() { return this.x } }
+class B extends A {
+  constructor() {
+    this.x = 3;
+  }
+}
+var res = new B().f();
+
 // arrow functions
 var f = (a, b) => a + b;
 
@@ -12,7 +33,7 @@ var f = (a, b) => a + b;
 //var arr = [1, 2];
 //var res = f(...arr);
 
-// rest parameter
+// rest parameters
 (function (a, ...args) { 
   return a + args[0] + args[1]; 
 }(1, 2, 3));
@@ -21,22 +42,8 @@ var f = (a, b) => a + b;
 (function ({a, b}) { return a + b; })({a:1, b:2});
 var [x, y] = [1, 2];
 
-// dynamic object keys
+// computed properties
 //var a = {b:1};
 //var c = {a.b: 0};
-
-
-// flow test
-function sum(a: number, b: number): number {
-  return a + b;
-}
-
-// CommonJS test
-var React = require('react');
-module.exports = sum;
-
-
-// JSX test
-var x = <div>hi</div>;
 
 console.log('yay!');
